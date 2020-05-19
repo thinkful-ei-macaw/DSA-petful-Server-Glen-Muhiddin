ba63f9d934c6f27cb0d0a40179d879a7c895f2d2
Muhiddin Kurbonov <muhiddin.m.kurbonov@gmail.com>
2020-05-18 22:19:48 -0700

    some change

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://petful-client-glen-muhiddin.now.sh',
+    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://petful-client-glen-muhiddin.now.sh/',
     PORT: process.env.PORT || 8000
 }
